18e7978044a9a1174f04aafd540a401ffdd76132
farhan0474 <farha11a@uwindsor.ca>
2023-10-12 23:38:17 -0400

    Added changePassword option

--- DAMS_Project/javascript/auth.js
+++ DAMS_Project/javascript/auth.js
@@ -2,93 +2,169 @@
 
 // Sign Up
 function SignUp() {
-   const email = document.getElementById("email").value;
-   const password = document.getElementById("password").value;
-   const confirmPassword = document.getElementById("passwordConfirm").value;
- 
-   if (!(password == confirmPassword)) {
-     window.alert("Password does not match!");
-   } else {
-
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-     
-    const data = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password
-
-    };
-
-    console.log(data);
- 
-     // Send a POST request to the server for user sign-up
-     fetch('/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Handle the response from the server
-        if (data.success) {
-          window.alert("You are Signed Up!");
-          localStorage.setItem('email', email);
-          window.location.href = "signIn.html";
-        } else {
-          window.alert("Error: " + data.error);
-        }
-      })
-      .catch((error) => {
-        console.error('Fetch error:', error);
-        window.alert('Fetch error: ' + error.message);
-      });
-   }
- }
- 
-// setting up firebase authentication for user login
-const firebaseApp = firebase.initializeApp({ 
-    apiKey: "AIzaSyAq_JJxWK6AGB5cGlxCVoWDItXTBvHoKyU",
-    authDomain: "dams-3565b.firebaseapp.com",
-    projectId: "dams-3565b",
-    storageBucket: "dams-3565b.appspot.com",
-    messagingSenderId: "665759459781",
-    appId: "1:665759459781:web:efda5177907e8e764c43ac",
-    measurementId: "G-7X1SY1V2NX"
-});
-
-const db = firebaseApp.fireStore();
-const auth = firebaseApp.auth();
- 
-// Sign In
-function SignIn() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  console.log(email, password);
+  const confirmPassword = document.getElementById("passwordConfirm").value;
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
- .then((result) => {
-   // Signed in
-     console.log("You are Signed In!");
-     console.log(result);
+  if (!(password == confirmPassword)) {
+    window.alert("Password does not match!");
+  } else {
 
-     // Storing the email locally for future reference
-     localStorage.setItem('email', email);
+   const firstName = document.getElementById("firstName").value;
+   const lastName = document.getElementById("lastName").value;
+    
+   const data = {
+     firstName: firstName,
+     lastName: lastName,
+     email: email,
+     password: password
 
-     window.location.href = "home.html";
-     // ...
- })
- .catch((error) => {
-     console.log(error.code);
-     console.log(error.message);
-     window.alert("Error: " + error.message);
- });
+   };
+
+   console.log(data);
+
+    // Send a POST request to the server for user sign-up
+    fetch('/signup', {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(data),
+   })
+     .then((response) => {
+       if (!response.ok) {
+         throw new Error('Network response was not ok');
+       }
+       return response.json();
+     })
+     .then((data) => {
+       // Handle the response from the server
+       if (data.success) {
+         window.alert("You are Signed Up!");
+         localStorage.setItem('email', email);
+         window.location.href = "signIn.html";
+       } else {
+         window.alert("Error: " + data.error);
+       }
+     })
+     .catch((error) => {
+       console.error('Fetch error:', error);
+       window.alert('Fetch error: ' + error.message);
+     });
+  }
+}
+
+// setting up firebase authentication for user login
+const firebaseApp = firebase.initializeApp({ 
+   apiKey: "AIzaSyAq_JJxWK6AGB5cGlxCVoWDItXTBvHoKyU",
+   authDomain: "dams-3565b.firebaseapp.com",
+   projectId: "dams-3565b",
+   storageBucket: "dams-3565b.appspot.com",
+   messagingSenderId: "665759459781",
+   appId: "1:665759459781:web:efda5177907e8e764c43ac",
+   measurementId: "G-7X1SY1V2NX"
+});
+
+//const db = firebaseApp.fireStore();
+const auth = firebaseApp.auth();
+
+// Sign In
+function SignIn() {
+ const email = document.getElementById("email").value;
+ const password = document.getElementById("password").value;
+ console.log(email, password);
+
+ firebase.auth().signInWithEmailAndPassword(email, password)
+.then((result) => {
+  // Signed in
+    console.log("You are Signed In!");
+    console.log(result);
+
+    // Storing the email locally for future reference
+    localStorage.setItem('email', email);
+
+    window.location.href = "home.html";
+    // ...
+})
+.catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+    window.alert("Error: " + error.message);
+});
+}
+
+function ChangeUserDetails(){
+
+ const newFirstName = document.getElementById('newFirstName').value;
+ const newLastName = document.getElementById('newLastName').value;
+ 
+ const user = firebase.auth().currentUser;
+
+ user.updateEmail(newEmail)
+   .then(() => {
+     console.log('User email updated successfully');
+   })
+   .catch((error) => {
+     console.error('Error updating user email:', error);
+   });
+
+ 
+}
+
+//Change password
+
+function UpdatePassword() {
+  const oldPassword = document.getElementById('oldPassword').value;
+  const newPassword = document.getElementById('newPassword1').value;
+  const confirmPassword = document.getElementById('confirmPassword1').value;
+
+  // Check if the new password and confirm password match
+  if (newPassword !== confirmPassword) {
+    alert('New password and confirm password do not match.');
+    return;
+  }
+
+  // Set up an observer to listen for changes in the authentication state
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // Reauthenticate the user with their old password
+      const credential = firebase.auth.EmailAuthProvider.credential(user.email, oldPassword);
+
+      user
+        .reauthenticateWithCredential(credential)
+        .then(function () {
+          // Change the password for the user
+          user
+            .updatePassword(newPassword)
+            .then(function () {
+              alert('Password updated successfully');
+
+              // Clear the password fields
+              const oldPasswordField = document.getElementById('oldPassword');
+              const newPasswordField = document.getElementById('newPassword1');
+              const confirmPasswordField = document.getElementById('confirmPassword1');
+
+              if (oldPasswordField) {
+                oldPasswordField.value = '';
+              }
+
+              if (newPasswordField) {
+                newPasswordField.value = '';
+              }
+
+              if (confirmPasswordField) {
+                confirmPasswordField.value = '';
+              }
+            })
+            .catch(function (error) {
+              alert('Error updating password: ' + error.message);
+            });
+        })
+        .catch(function (error) {
+          alert('Error reauthenticating user: ' + error.message);
+        });
+    } else {
+      alert('User not authenticated. Please sign in.');
+    }
+  });
 }
