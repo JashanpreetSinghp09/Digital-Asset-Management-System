f06367c3c0d91417941a1115d3ad4470aa7462ff
farhan0474 <farha11a@uwindsor.ca>
2023-11-27 21:32:40 -0500

    added prompts for uploading

--- DAMS_Project/javascript/server.js
+++ DAMS_Project/javascript/server.js
@@ -197,7 +197,13 @@ app.get('/getUid', async (req, res) => {
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
-
+  try {
+    // Assuming the upload was successful
+    res.status(200).json({ message: 'File uploaded successfully!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 
