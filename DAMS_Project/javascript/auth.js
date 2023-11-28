ba306c2c81a67a5645086e1a3ce2326476bced38
ibrahim <mahmoud2@uwindsor.ca>
2023-11-27 19:58:07 -0500

    bugs fix

--- DAMS_Project/javascript/auth.js
+++ DAMS_Project/javascript/auth.js
@@ -444,6 +444,79 @@ function generateAssetHTML(asset) {
   return assetElement;
 }
 
+function generatePublicAssetHTML(asset) {
+  const assetElement = document.createElement('div');
+  assetElement.classList.add('asset-item');
+
+  // Determine the asset type based on the file extension or contentType
+  let mediaTag, width, height;
+  if (asset.contentType.startsWith('image/')) {
+    mediaTag = 'img';
+    width = '500';
+    height = '500';
+  } else if (asset.contentType.startsWith('video/')) {
+    mediaTag = 'video';
+    width = '500';
+    height = '500';
+  } else if (asset.contentType.startsWith('audio/')) {
+    mediaTag = 'audio';
+  } else if (asset.contentType === 'application/pdf') {
+    // For PDF files, use an iframe
+    mediaTag = 'iframe';
+    width = '100%';
+    height = '500px'; // Adjust the height as needed
+  } else {
+    // For other file types, use an image tag by checking the file extension
+    const fileExtension = asset.filename.split('.').pop().toLowerCase();
+    if (['jpg', 'jpeg', 'png', 'gif'].includes(fileExtension)) {
+      mediaTag = 'img';
+      width = '500';
+      height = '500';
+    } else if (['mp3'].includes(fileExtension)) {
+      mediaTag = 'audio';
+    } else if (['mp4'].includes(fileExtension)) {
+      mediaTag = 'video';
+      width = '500';
+      height = '500';
+    } else {
+      // If the type is not recognized, default to an image tag
+      mediaTag = 'img';
+      width = '500';
+      height = '500';
+    }
+  }
+
+  // Create the media element based on the asset type
+  const mediaElement = document.createElement(mediaTag);
+  mediaElement.src = `/download/${asset._id}`;
+  mediaElement.alt = asset.filename;
+  mediaElement.controls = true;
+
+  // Set width and height attributes
+  mediaElement.width = width;
+  mediaElement.height = height;
+
+  // Create a link to the download route
+  const downloadLink = document.createElement('a');
+  downloadLink.href = `/download/${asset._id}`;
+  downloadLink.target = '_blank';
+
+  // Create a paragraph to display the filename
+  const filenameParagraph = document.createElement('p');
+  filenameParagraph.textContent = asset.filename;
+
+  // Append the media element to the asset element
+  assetElement.appendChild(mediaElement);
+
+  // Append the link to the asset element
+  assetElement.appendChild(downloadLink);
+
+  // Append the filename paragraph to the link
+  downloadLink.appendChild(filenameParagraph);
+
+  return assetElement;
+}
+
 
 // Function to delete an asset on the server
 async function deleteAsset(assetId) {
@@ -604,7 +677,7 @@ async function displayPublicAssets(category = null) {
 
             // Loop through the assets in the category and generate HTML for each
             assetsInCategory.forEach((asset) => {
-              const assetElement = generateAssetHTML(asset);
+              const assetElement = generatePublicAssetHTML(asset);
               categoryContainer.appendChild(assetElement);
             });
 
@@ -624,7 +697,7 @@ async function displayPublicAssets(category = null) {
 
             // Loop through the assets in the category and generate HTML for each
             assets.forEach((asset) => {
-              const assetElement = generateAssetHTML(asset);
+              const assetElement = generatePublicAssetHTML(asset);
               categoryContainer.appendChild(assetElement);
             });
 
@@ -684,7 +757,7 @@ function filterPublicAssets(category = null) {
 
             // Loop through the assets in the category and generate HTML for each
             assetsInCategory.forEach((asset) => {
-              const assetElement = generateAssetHTML(asset);
+              const assetElement = generatePublicAssetHTML(asset);
               categoryContainer.appendChild(assetElement);
             });
 
@@ -704,7 +777,7 @@ function filterPublicAssets(category = null) {
 
             // Loop through the assets in the category and generate HTML for each
             assets.forEach((asset) => {
-              const assetElement = generateAssetHTML(asset);
+              const assetElement = generatePuclicAssetHTML(asset);
               categoryContainer.appendChild(assetElement);
             });
 
